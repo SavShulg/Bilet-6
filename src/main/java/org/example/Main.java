dbb845fd1e13edb027531b1a2275fb717c85f17b
Home <zavalnyy_saveliy@mail.ru>
2024-05-27 09:52:34 +1000

    complete!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("C://Users//Desktop//text"));
+
+            String firstString = reader.readLine();
+            System.out.println(firstString);
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка! Файл не найден!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
